b505512be6d8241aad5bf7f638607ec5c96bdb2f
Kushagra Verma <verma.k@husky.neu.edu>
2016-02-07 13:04:22 -0500

    added trials for flattening binary trees

--- src/arrays/FindPivotDemo.java
+++ src/arrays/FindPivotDemo.java
@@ -3,13 +3,36 @@ package arrays;/* Authored by Kushagra on 2/7/2016. */
 public class FindPivotDemo {
     public static void main(String[] args) {
 
-        int[] A = {};
+        int[] A = {5, 6, 1, 2, 3, 4};
 
         System.out.println(findPivot(A));
 
     }
 
     private static int findPivot(int[] A) {
+
+        int left = 0;
+        int right = A.length - 1;
+
+        while (left <= right) {
+
+            int leftElement = A[left];
+            int rightElement = A[right];
+
+            int middle = (left + right) / 2;
+
+            // terminal step found here
+            if (middle < right && A[middle] > A[middle + 1]) {
+                return A[middle  + 1];
+            }
+            else if (leftElement > rightElement) {
+                left = middle + 1;
+            }
+            else {
+                right = middle - 1;
+            }
+        }
+
         return 0;
     }
 }
---
+++ src/convertds/flattenBinaryTreeDemo.java
@@ -0,0 +1,77 @@
+package convertds;/* Authored by Kushagra on 2/7/2016. */
+
+public class flattenBinaryTreeDemo {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        createDemoTree(root);
+//        printTree(root);
+
+        root = flattenBinaryTree(root);
+
+        printFlattenedTree(root);
+
+    }
+
+    private static void printFlattenedTree(TreeNode root) {
+        System.out.println("Printing Flattened Tree");
+
+        if (root == null) return;
+
+        TreeNode node = root;
+        while (node != null) {
+            System.out.println(node.key);
+            node = node.right;
+        }
+    }
+
+    // trying out a recursive solution approach
+    private static TreeNode flattenBinaryTree(TreeNode root) {
+
+        if (root == null) return null;
+
+        TreeNode rightSubTree = root.right;
+        root.right = flattenBinaryTree(root.left);
+        root.left = null;
+
+        TreeNode node = root.right;
+        if (node != null) {
+            while (node.right != null) {
+                node = node.right;
+            }
+
+            node.right = flattenBinaryTree(rightSubTree);
+        }
+
+        return root;
+    }
+
+    private static void printTree(TreeNode root) {
+        if (root == null) return;
+
+        System.out.println(root.key);
+        printTree(root.left);
+        printTree(root.right);
+    }
+
+    private static void createDemoTree(TreeNode root) {
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = null;
+        root.right.right = new TreeNode(6);
+    }
+
+    static class TreeNode {
+        int key;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
